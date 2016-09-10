265cfa31dbb77408017ff49112b95892d6b9c19c
Michael Haney <michael.haney@colorado.edu>
2016-09-10 13:30:24 -0600

    different asset path for bg image

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.precompile += ['js/*']
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
